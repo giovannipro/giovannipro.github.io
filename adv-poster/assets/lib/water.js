b82a8b02a1992bc563e24b2d3310f18842eef38c
Giovanni Profeta <giovanni@NBMC-662.dacd.supsi.ch>
2016-11-19 03:10:11 +0100

    test

--- adv-poster/assets/lib/water.js
+++ adv-poster/assets/lib/water.js
@@ -1,19 +1,19 @@
 function get_data(param){
 
-    var cross_origin = 'https://crossorigin.me/'
-    var api = 'http://www.oasi.ti.ch/web/rest/measure?domain=air&resolution=h&parameter=pm10&locations=Nabel_LUG&from='
+    var cross_origin = "https://crossorigin.me/",
+        api = "http://www.oasi.ti.ch/web/rest/measure?domain=air&resolution=h&locations=Nabel_LUG";
+    
+    var d = new Date(),
+        year = d.getFullYear(),
+        month = d.getMonth() + 1,
+        day = d.getDate();
+        hour = d.getHours();
 
-    var d = new Date();
-    var year = d.getFullYear();
-    var month = d.getMonth() + 1;
-    var day = 18; //d.getDate();
-    var hour = 18; //d.getHours();
+    var time = year + month + day + "T" + hour,
+        date = year + "-" + month + "-" + day + "&" + year + "-" + month + "-" + day,
+        request = api + "&parameter=" + param + "&from=" + date;
 
-    var time = year +''+ month +''+ day + 'T' + hour
-    var date = year+'-'+month+'-'+day+'&'+year+'-'+month+'-'+day
-    var request = api + date;
-
-    console.log(request)
+    console.log(request);
 
     $.ajax({
         url: cross_origin + request,
@@ -34,39 +34,65 @@ function get_data(param){
         }
 
         dates = $(data.locations)[0].data;
-        x = $(data.locations)[0].data[0].date; //.data[0].values[0].date;
         //console.log(dates)
-        //console.log(x)
+        x = $(data.locations)[0].data[0].date;
+
+        if (hour < 10) {
+            hour = "0" + (hour - 1);
+        }
+        //console.log(hour);
 
         jQuery.each( dates, function( a,b ) {
 
-            var date_string = b.date.toString().substring(8,13)
-            var match = day + 'T' + (hour)
-            //console.log(date_string)
+            var date_string = b.date.toString().substring(0,13);
 
-            if (date_string == match) {
-                //console.log(b.date + '-' + b.values[0].value)
-                if (b.values[0].value != null) {
-                    console.log(b.date)
-
-                    var value =  b.values[0].value,
-                        percentage = (value * 100) / max;
-                    //wave_maker(value,min,max);
-                    console.log(param + ":" + value + "/" + max + " (" + percentage + "%)");
-                    
-                    return false;
-                }
+            if (hour == 0){
+                match = year + "-" + month + "-" + (day-1) + "T23";
+                console.log(match)
             }
             else{
-                //console.log('error')
+                if (hour < 10) {
+                    match = year + "-" + month + "-" + day + "T0" + (hour-1);
+                }
+                else{
+                    match = year + "-" + month + "-" + day + "T" + (hour-1);
+                }
+                //console.log(match)
             }
-        })        
+
+            if (date_string == match) {
+                //console.log(date_string + ">" + match);
+                var value =  b.values[0].value;
+
+                if (value !== null) { // b.values[0].value
+                   
+                    var percentage = (value * 100) / max;
+
+                    $('#no_data').empty();
+                    wave_maker(value,min,max);
+
+                    console.log(date_string + ' - ' + param + ":" + value + "/" + max + " (" + percentage.toFixed(0) + "%)");
+
+                    return false; 
+                }
+                else{
+                    $('#no_data').empty();
+                    $('#no_data').append('<div style="height100%;">no data available <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></div>');
+                    wave_maker(0,min,max);
+                    //console.log('no data')
+                    return false; 
+                }
+            }
+            
+            else{
+                //console.log("error: " + match)
+            }
+        }) ;       
         save(time);
     })
     .fail(function() {
-        water(0,0,100);
-    })
-
+        wave_maker(0,0,100);
+    });
 }
 
 
@@ -89,37 +115,35 @@ sea variables
 var waves = 20,
 v_translation = ((height/10) / waves);
 
+function wave_maker(index,min,max){
+    var data = [];
+    for (var i=0; i<waves; i++) { 
+        data.push({
+            x: i ,
+            y: Math.random()
+        });
+    }
+    //console.log(data)
+    var value = (250/max) * index;
+    water(data,value);
+}
+
 /* -----------------------
 set plot
 ------------------------- */
 
-function water(index,min,max){
-
-    var value = (600/max) * index // 0 - 600
-    //console.log(value)
+function water(data,value){
+    //console.log(data)
 
     var svg = d3.select("#svg_container")
         .append("svg")
         .attr("id", "svg")
-        .attr("viewBox", '0 0 ' + width + ' ' + (height) )
-        .attr("fill","#1625B8")
+        .attr("viewBox", "0 0 " + width + " " + (height) )
+        .attr("fill","#1625B8");
 
     var plot = svg.append("g")
         .attr("id", "d3_plot");
         //.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    /* -----------------------
-    get data
-    ------------------------- */
-
-    var data = [];
-    for (var i=0; i<waves; i++) { 
-    	data.push({
-    		x: i ,
-    		y: Math.random(0, value)
-    	})
-    }
-    //console.log(data)
 
     /* -----------------------
     scale
@@ -129,70 +153,60 @@ function water(index,min,max){
     var max_y = d3.max(data, function(d) { return +d.y ;} );
 
     var xScale = d3.scale.linear()
-    	.domain([0,max_x])
-    	//.range([0,width - margin.left - margin.right])
-        .range([0,width])
+        .domain([0,max_x])
+        //.range([0,width - margin.left - margin.right])
+        .range([0,width]);
 
     var yScale = d3.scale.linear()
-    	.domain([0,max_y])
-    	.range([height - margin.top - margin.bottom,0])
+        .domain([0,max_y])
+        .range([height - margin.top - margin.bottom,0]);
 
     /* -----------------------
     visualize elements
     ------------------------- */
 
     var line = d3.svg.line()
-    	.interpolate('basis')
+        .interpolate("basis")
         .x(function(d){
-        	return xScale(d.x)
+            return xScale(d.x);
         })
         .y(function(d){
-        	return (d.y * ( ((Math.random() * value) ) ) ) 
+            return (d.y * ( ((Math.random() * value ) ) ) ) ;
         });
 
-    var onde = plot.selectAll('.wave')
+    var onde = plot.selectAll(".wave")
         .data(data)
         .enter()
-        .append('g')
-        .attr('transform',function(d,i){
-        	return 'translate(0,' +  ( v_translation * (Math.exp(i/3) ) ) + ')'
+        .append("g")
+        .attr("transform",function(d,i){
+            return "translate(0," +  ( v_translation * (Math.exp(i/3))) + ")"
         }) // i/3
-        .attr('class',function(d,i){
-        	return i
+        .attr("class",function(d,i){
+            return i
         })
-        .append('g')
-        .attr('transform',function(d,i){
-        	return 'scale(1,' + ((i/10)) + ')'
+        .append("g")
+        .attr("transform",function(d,i){
+            return "scale(1," + ((i/10)) + ")"
         })
         .append("path")
         .attr("d", function(d,i){
-        	return line(data)
+            return line(data)
         })
         .attr("class", "line_a")
-        .attr("stroke-dasharray","1, 4")
+        //.attr("stroke-dasharray","1, 4")
+        .attr("opacity","0.6")
         .style("stroke", "white" )
-        .attr('stroke-width','2px')
-        .attr('fill', 'none')
+        .attr("stroke-width","2px")
+        .attr("fill", "none")
 }
 
 function save(time){
-    $('#save').click(function () {
+    $("#save").click(function () {
 
-        var dataviz = $('#svg_container').html();
+        var dataviz = $("#svg_container").html();
         //console.log(dataviz);
         download(dataviz, "pm10_lugano_" + time +".svg", "text/plain");
     });   
-}
-
-function reload(){
-    setInterval(function(){
-        $("#svg_container").hide()
-        $("#svg_container").empty()
-    },3000);
-        setInterval(function(){
-        wave_maker();
-        $("#svg_container").show()
-    },3000);
 }
 
 $( document ).ready(function() {
