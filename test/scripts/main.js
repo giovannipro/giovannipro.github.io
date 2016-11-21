539fa733215404ce5457ce47664fec077e35bfa8
Giovanni Profeta <giovanni@NBMC-662.dacd.supsi.ch>
2016-11-21 02:33:20 +0100

    Test: added React
    some bugs

--- test/scripts/main.js
+++ test/scripts/main.js
@@ -1,19 +1,44 @@
 require.config({
-    baseUrl: 'bower_components',
+    baseUrl: "bower_components",
     paths : {
-        'jquery': 'jquery/dist/jquery.min',
-        'bootstrap': 'bootstrap/dist/js/bootstrap.min',
-        'd3': 'd3/d3.min',
-        'canvas-toBlob': 'canvas-toBlob/canvas-toBlob',
+        "jquery": "jquery/dist/jquery.min",
+        "bootstrap": "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min",
+        "d3": "d3/d3.min",
+        "canvas-toBlob": "canvas-toBlob/canvas-toBlob",
 
-        'functions':'../scripts/functions'      
+        "react-dom":"react/react-dom",
+        "react": "react/react-with-addons.min",
+        "babel": "requirejs-react-jsx/babel-5.8.34.min",
+        "jsx":"requirejs-react-jsx/jsx",
+        "text":"requirejs-text/text",
+
+        "functions":"../scripts/functions"
+    },
+    shim : { // dependencies
+        "functions" : {
+            "deps" :["jquery"] 
+        },
+        "react": {
+            "exports": "React"
+        }
+    },
+    config: {
+        babel: {
+            sourceMaps: "inline", // One of [false, "inline", "both"]. See https://babeljs.io/docs/usage/options/
+            fileExtension: ".jsx" // Can be set to anything, like .es6 or .js. Defaults to .jsx
+        }
     }
 });
-require([
-    'jquery',
-    'bootstrap',
-    'd3',
-    'canvas-toBlob',
 
-    'functions'
+require([
+    "react-dom",
+    "functions"
 ]);
+
+/*
+require(["jsx!../scripts/app"], function(App){
+    var app = new App();
+    app.init();
+});
+
+*/
