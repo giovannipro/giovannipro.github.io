2b7fc242062f3c9e99a471c952a62e38d4663548
giovannipro <giovanni.profeta@supsi.ch>
2020-09-14 18:30:34 +0200

    update dataviz about most viewed articles

--- wikipedia-primary-school-italy/assets/script/dv2.js
+++ wikipedia-primary-school-italy/assets/script/dv2.js
@@ -1,12 +1,12 @@
-function dv1() {
-	let container = "#dv1",
-		window_w = $(container).outerWidth() * 1, // 4000; //
-		window_h = window.innerHeight * 1 //2000;
+function dv2() {
+	let multiply = 1;
+	let container = "#dv2",
+		window_w = $(container).outerWidth() * multiply,
+		window_h = window.innerHeight * multiply;
 
 	let margin = {top: 20, right: 20, bottom: 20, right: 20},
 		width = window_w - (margin.right + margin.right),
 		height = window_h - (margin.top + margin.bottom);
-
 
 	function render(){
 		$(container).empty();
@@ -23,6 +23,7 @@ function dv1() {
 
 		d3.tsv("../assets/data/articles.tsv", function (error, data) {
 			if (error) throw error;
+			// console.log(data)
 
 			let tab = "&#09;";
 			let br = ""
@@ -30,14 +31,21 @@ function dv1() {
 			let dataset = [];
 			dataset.push(head)
 
-			let filter = 1001; // 2000;
+			let filter = 10;
 			let total = 0;
 			let filtered_data = [];
 			let subjects = [];
 			let art = []
 			let duplicates = [];
-			data.forEach(function (d) {
-				if (d.average_daily_visit > filter){
+
+			let grouped_data = groupByKey(data, "subject");
+			// console.log(grouped_data)
+
+			// grouped_data.forEach(function (i,d) {
+			for (const [d,c] of Object.entries(grouped_data)) {
+				// console.log(value)
+
+				c.forEach(function (d) {
 					d.average_daily_visit = +d.average_daily_visit
 					d.incipit_size = +d.incipit_size
 					d.incipit_on_size = +d.incipit_on_size
@@ -45,63 +53,73 @@ function dv1() {
 					d.year = +d.year
 					d.subject = d.subject
 					d.discussion_size = +d.discussion_size
+					d.issues = +d.issues
+					// d.discussion_on_size = (d.size/d.discussion_size).toFixed(1)
+				})
 
-					// if (d.discussion_size == "NaN") {
-					console.log(d.discussion_size + " " + d.article)
-					// }
+				c.sort(function(a, b) {
+				    return b.average_daily_visit - a.average_daily_visit;
+				});
+			}
+			// console.log(grouped_data)
+			
+			for (const [d,c] of Object.entries(grouped_data)) {
+				c.forEach(function (d,i) {
+					if (i <= filter){
+						filtered_data.push(d)
+						total += 1
 
-					total += 1
-					filtered_data.push(d)
+						// duplicates
+						if (art.includes(no_underscore(d.article))) {
+							duplicates.push(no_underscore(d.article))
+						}
+						else {
+							art.push(no_underscore(d.article))
+						}
 
-					if (subjects.includes(d.subject)) {}
-					else {subjects.push(d.subject)}
+						if (d.discussion_size == 0 || d.discussion_size == NaN) {
+							// console.log("size: " + d.discussion_size + " " + no_underscore(d.article))
+						}
 
-					// duplicates
-					if (art.includes(d.article)) {
-						duplicates.push(d.article)
+						// if (d.issues > 0) {
+						// 	console.log(d.issues + " " + no_underscore(d.article))
+						// }
+
+						let output = d.article + "," + d.subject + "," + d.average_daily_visit + "," + d.incipit_size + "," + d.size + "," + d.year + "<br/>"; 
+						dataset.push(output)
 					}
-					else {
-						art.push(d.article)
-					}
+				})
+			}
+			console.log(total);
 
-					let output = d.article + "," + d.subject + "," + d.average_daily_visit + "," + d.incipit_size + "," + d.size + "," + d.year + "<br/>"; 
-					dataset.push(output)
-				}
-			})
+			$("#dv2_dataset").append(dataset)
+
+			filtered_data.sort(function (a, b) { 
+	            var af = a.subject; 
+	            var bf = b.subject; 
+	            var as = a.article; 
+	            var bs = b.article; 
+	              
+	            if(af == bf) { 
+	                return (as < bs) ? -1 : (as > bs) ? 1 : 0; 
+	            } else { 
+	                return (af < bf) ? -1 : 1; 
+	            } 
+	        }); 
 
 			let test_a = {article:'A_9000',subject:'Biologia', average_daily_visit:9000, incipit_size:50000, size:25000, year:2, discussion_size:100000};
-			let test_b = {article:'A_0',subject:'Biologia', average_daily_visit:0, incipit_size:70000, size:35000, year:0, discussion_size:100000};
+			let test_b = {article:'B_0',subject:'Biologia', average_daily_visit:0, incipit_size:70000, size:35000, year:0, discussion_size:100000};
 			filtered_data.push(test_a);
 			filtered_data.push(test_b);
 
-			// filtered_data.sort(function (a, b) { 
-	  //           var af = a.subject; 
-	  //           var bf = b.subject; 
-	  //           var as = a.article; 
-	  //           var bs = b.article; 
-	              
-	  //           if(af == bf) { 
-	  //               return (as < bs) ? -1 : (as > bs) ? 1 : 0; 
-	  //           } else { 
-	  //               return (af < bf) ? -1 : 1; 
-	  //           } 
-	  //       });
-
-	       sortByKey(filtered_data, "article") 
-
-			// console.log(filtered_data)
-			console.log("article: " + total)
-			console.log(subjects)
-			// console.log(filtered_data[118])
-			// console.log(filtered_data[117])
-
 			if (duplicates === undefined || duplicates.length > 0) {
-    			console.log(duplicates + " duplicates")
+				duplicates.forEach(function (d,i) {
+    				console.log(d)
+				})
 			}
 			else {
 				console.log("no duplicates")
 			}
-
 
 			$("#dv1_dataset").append(dataset)
 
@@ -151,20 +169,18 @@ function dv1() {
 					return r(Math.sqrt(d.size/3.14)) 
 				})
 				.attr("fill", function(d,i){
-					return apply_color(d.subject)
-					// return color(d.subject)
+					return color(d.subject)
 				})
 				.attr("opacity",0.5)
 
 			let incipit = articles.append("circle")
 				.attr("cx",0)
-				
+				.attr("cy",0)
 				.attr("r", function(d,i){
 					return r(Math.sqrt(d.incipit_size/3.14))
 				})
 				.attr("fill", function(d,i){
-					return apply_color(d.subject)
-					// return color(d.subject)
+					return color(d.subject)
 				})
 				.attr("opacity",0.5)
 
@@ -175,31 +191,40 @@ function dv1() {
 					return r(Math.sqrt(d.discussion_size/3.14))
 				})
 				.attr("stroke", function(d,i){
-					return apply_color(d.subject)
-					// return color(d.subject)
+					return color(d.subject)
 				})
 				.attr("fill","transparent")
 				.attr("stroke-width",0.5)
 				.attr("opacity",0.9)
 
+			let issues = articles.append("g")
+				// .style("transform","rotate(-45deg)")
+				.append("rect")
+				.attr("x",0)
+				.attr("y",0)
+				.attr("height",3)
+				.attr("width", function(d,i){
+					return d.issues*20
+				})
+				.attr("fill", function(d,i){
+					return color(d.subject)
+				})
+				.attr("opacity",0.9)
+
 			let label = articles.append("text")
 				.text(function(d,i){
-					return no_underscore(d.article) // + " " + d.year //+ " " + d.average_daily_visit
+					return no_underscore(d.article) // + " " + d.year //+ " " + d.discussion_size // + " " + d.year //+ " " + d.average_daily_visit
 				})
 				.attr("x",0)
 				.attr("y",-5)
 				.attr("text-anchor","middle")
 				.attr("fill","black")
 				.attr("font-size","0.6em")
-
-			// let yAxis = plot.append("g")
-			// 	.attr("transform", "translate(0,0)")
-			// 	.call(d3.axisLeft(y));
 		})
 	}
 	render();
 }
 
 $(document).ready(function() {
-	dv1();
+	dv2();
 });
--- wikipedia-primary-school-italy/assets/script/utilities.js
+++ wikipedia-primary-school-italy/assets/script/utilities.js
@@ -12,3 +12,58 @@ function no_underscore(item){
 	let new_item = item.replaceAll("_"," ")
 	return new_item
 }
+
+function apply_color(item){
+	var color = "";
+
+	if (item == "Biologia" || item == "Chimica" || item == "Fisica" || item == "Scienze della Terra"){ // scienze naturali
+		color = "green";
+	}
+	else if (item == "Filosofia" || item == "Letteratura italiana" || item == "Storia" || item == "Storia dell'arte"){ // scienze umanistiche
+		color =  "violet";
+	}
+	else if (item == "Matematica" || item == "Informatica"){ // scienze formali
+		color =  "blue";
+	}
+	else { // scienze sociali
+		color = "gray";
+	}
+	
+	// if (item == "Geografia"){
+	// 	color = "red";
+	// }
+	// else if (item == "Storia"){
+	// 	color = "green";
+	// }
+	// else if (item == "Letteratura italiana"){
+	// 	color =  "violet";
+	// }
+	// else if (item == "Matematica"){
+	// 	color =  "orange";
+	// }
+	// else if (item == "Cittadinanza e costituzione"){
+	// 	color =  "blue";
+	// }
+	// else if (item == "Storia dell'arte"){
+	// 	color =  "yellow";
+	// }
+	// else {
+	// 	color = "gray";
+	// }
+	return color;
+}
+
+function groupByKey(array, key) {
+   return array
+     .reduce((hash, obj) => {
+       if(obj[key] === undefined) return hash; 
+       return Object.assign(hash, { [obj[key]]:( hash[obj[key]] || [] ).concat(obj)})
+     }, {})
+}
+
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
