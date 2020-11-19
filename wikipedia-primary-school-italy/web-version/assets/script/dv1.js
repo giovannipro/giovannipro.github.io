f1d8a926634f64ddc58b560ee72e30809f9adda7
giovannipro <giovanni.profeta@supsi.ch>
2020-11-19 01:02:55 +0100

    enhance mouseover

--- wikipedia-primary-school-italy/web-version/assets/script/dv1.js
+++ wikipedia-primary-school-italy/web-version/assets/script/dv1.js
@@ -3,9 +3,29 @@ function apply_color(item){
 	return color;
 }
 
+const subjects = [
+	"Biologia",
+	"Chimica",
+	"Cittadinanza e costituzione",
+	"Diritto e Economia",
+	"Filosofia",
+	"Fisica",
+	"Geografia",
+	"Grammatica italiana",
+	"Grammatica latina",
+	"Informatica",
+	"Letteratura italiana",
+	"Letteratura latina",
+	"Matematica",
+	"Scienze della Terra",
+	"Scienze",
+	"Storia",
+	"Tecnologia"
+]
+
 const container = "#dv1";
-let filtered_data = [];
 const font_size = 12;
+const filter_item = 100;
 
 let multiply = 1;
 let window_w = $(container).outerWidth();
@@ -14,8 +34,6 @@ let window_w = $(container).outerWidth();
 let margin = {top: 20, left: 20, bottom: 20, right: 20},
 	width = window_w - (margin.right + margin.right),
 	height = window_h - (margin.top + margin.bottom);
-
-let filter_item = 500;
 
 let svg = d3.select(container)
 	.append("svg")
@@ -29,10 +47,10 @@ let plot = svg.append("g")
 
 function dv1(the_subject) {
 	d3.tsv("../assets/data/articles.tsv").then(loaded)
-	d3.select("#articles").remove();
-	d3.select("#yAxis").remove();
 
 	function loaded(data) {
+
+		// load data
 		let total = 0;
 
 		let subject_articles;
@@ -66,13 +84,13 @@ function dv1(the_subject) {
 			d.id = +d.id
 			d.discussion_size = +d.discussion_size
 			d.average_daily_visit = +d.average_daily_visit
+			d.article = d.article.replace("_"," ")
 		})
-		// console.log(total,filtered_data)
 		
 		// scale
-		let y_min = d3.min(filtered_data, function(d) { 
-			return d.average_daily_visit;
-		})
+		// let y_min = d3.min(filtered_data, function(d) { 
+		// 	return d.average_daily_visit;
+		// })
 		let y_max = d3.max(filtered_data, function(d) { 
 			return d.average_daily_visit;
 		})
@@ -93,6 +111,7 @@ function dv1(the_subject) {
 			.range([0, 20])
 			.domain([0,r_max])
 
+		// plot data
 		let articles = plot.append("g")	
 			.attr("id","articles")
 
@@ -121,8 +140,7 @@ function dv1(the_subject) {
 			
 		let article_text = article.append("g")
 			.attr("class","article_text")
-			.attr("opacity",0)
-
+			
 		let circles = article_circles.append("circle")
 			.transition()
 			.duration(500)
@@ -187,54 +205,222 @@ function dv1(the_subject) {
 				return d.article // cropText(no_underscore()) // + " " + d.year //+ " " + d.discussion_size // + " " + d.year //+ " " + d.average_daily_visit
 			})
 			.attr("x",0)
-			.attr("y",-20)
+			.attr("y",-height)
 			.attr("text-anchor","middle")
 			.attr("fill","black")
 			.attr("font-size",font_size)
 			.attr("class","text")
+			.attr("opacity",0)
 			
 		function handleMouseOver(d, i) { 
-			d3.select(this.parentNode).select(".article_text")
+			d3.selectAll(".article")
+				.attr("opacity",0.2)
+
+			d3.select(this.parentNode)
 				.attr("opacity",1)
+
+			d3.select(this.parentNode).select("text") // .select(".article_text")
+				.attr("opacity",1)
+				.attr("y",-20)
+
+			// console.log(d3.select(this.parentNode).select("text") )
 		}
 
 		function handleMouseOut(d, i) { 
-			d3.select(this.parentNode).select(".article_text")
+			d3.selectAll(".article")
+				.attr("opacity",1)
+
+			d3.select(this.parentNode).select("text") // .select(".article_text")
 				.attr("opacity",0)
+				.attr("y",-height)	
 		}
 
 		let yAxis = plot.append("g")
 			.attr("id","yAxis")
 			.attr("transform", "translate(" + (margin.left) + "," + (margin.top) +")")
 			.call(d3.axisLeft(y));
+
+		$( "#subjects" ).change(function() {
+			let subject = this.value;
+			update(subject);
+		});
+
+		function update(the_subject){
+			// console.log(the_subject)
+
+			d3.selectAll("circle")
+				.transition()
+				.duration(300)
+				.attr("r",0)
+
+			// load data
+			let total = 0;
+
+			let subject_articles;
+			let visit_sort;
+			let filter_data;
+
+			let subject_group = d3.nest()
+				.key(d => d.subject)
+				.entries(data)
+		
+			for (const [d,c] of Object.entries(subject_group)) {
+				if (c.key == the_subject){
+					subject_articles = c.values;
+				}
+			}
+			
+			visit_sort = subject_articles.sort(function(x, y){
+				return d3.descending(+x.average_daily_visit, +y.average_daily_visit);
+			})
+
+			filter_data = visit_sort.filter(function(x,y){ 
+				return y < filter_item 
+			})
+
+			filtered_data = filter_data.sort(function(a, b){
+				return d3.ascending(a.article, b.article);
+			})
+		
+			filtered_data.forEach(function (d,i) {
+				total += 1
+				d.id = +d.id
+				d.discussion_size = +d.discussion_size
+				d.average_daily_visit = +d.average_daily_visit
+				d.article = d.article.replace("_"," ")
+			})
+			// console.log(total)
+
+			// scale
+			let y_max = d3.max(filtered_data, function(d) { 
+				return d.average_daily_visit;
+			})
+
+			let x = d3.scaleLinear()
+				.domain([0,total])
+				.range([0,width-margin.right-margin.left-20]) 
+
+			let y = d3.scaleLinear()
+				.domain([0,y_max+(y_max/100*10)]) 
+				.range([height-margin.top,0])
+
+			svg.select("#yAxis")
+				.transition()
+				.call(d3.axisLeft(y));
+
+			// plot data
+			let articles = plot.append("g")	
+				.attr("id","articles")
+
+			let article = articles.selectAll("g")
+				.data(filtered_data)
+				.enter()
+				.append("g")
+				.attr("transform","translate(0,0)")	
+				.attr("class","article")
+				.attr("id", function(d,i){
+					return i
+				})
+				.attr("transform", function(d,i){
+					return "translate(" + (x(i)+50) + "," + y(d.average_daily_visit) + ")"
+				})
+
+			let article_circles = article.append("g")
+				.attr("class","article_circles")
+				.on("mouseover", handleMouseOver)
+				.on("mouseout", handleMouseOut)
+				.append("a")
+				.attr("xlink:href", function(d,i){
+					return "https://it.wikipedia.org/wiki/" + d.article
+				})
+				.attr("target","_blank")
+				
+			let article_text = article.append("g")
+				.attr("class","article_text")
+				// .attr("opacity",0)
+
+			let circles = article_circles.append("circle")
+				.transition()
+				.duration(500)
+				.delay(function(d,i){ 
+					return i * 2
+				})
+				.attr("cx",0)
+				.attr("cy",0)
+				.attr("fill", function(d,i){
+					return apply_color(d.subject)
+				})
+				.attr("opacity",0.5)
+				.attr("r",0)
+				.transition()
+				.ease(d3.easeLinear)
+				.duration(500) 
+				.attr("r", function(d,i){
+					return r(Math.sqrt(d.size/3.14)) 
+				})
+
+			let incipit = article_circles.append("circle")
+				.transition()
+				.duration(500)
+				.delay(function(d,i){ 
+					return i * 2
+				})
+				.attr("cx",0)
+				.attr("cy",0)
+				.attr("fill", function(d,i){
+					return apply_color(d.subject)
+				})
+				.attr("opacity",0.5)
+				.attr("r", function(d,i){
+					return r(Math.sqrt(d.incipit_size/3.14))
+				})
+
+			let discussion = article_circles.append("circle")
+				.transition()
+				.duration(500)
+				.delay(function(d,i){ 
+					return i * 2
+				})
+				.attr("cx",0)
+				.attr("cy",0)
+				.attr("stroke", function(d,i){
+					return apply_color(d.subject)
+				})
+				.attr("fill","transparent")
+				.attr("stroke-width",0.5)
+				.attr("opacity",0.9)
+				.attr("r",0)
+				.transition()
+				.delay(500)
+				.ease(d3.easeLinear)
+				.duration(500) 
+				.attr("r", function(d,i){
+					return r(Math.sqrt(d.discussion_size/3.14))
+				})
+
+			let label = article_text.append("text")
+				.text(function(d,i){
+					return d.article // cropText(no_underscore()) // + " " + d.year //+ " " + d.discussion_size // + " " + d.year //+ " " + d.average_daily_visit
+				})
+				.attr("x",0)
+				.attr("y",-height)
+				.attr("text-anchor","middle")
+				.attr("fill","black")
+				.attr("font-size",font_size)
+				.attr("class","text")
+		}
 	}
-
-	// function update_dv1(subject){
-	// 	let data = get_data()
-
-	// 	let y_min = d3.min(data, function(d) { 
-	// 		return d.average_daily_visit
-	// 	})
-	// 	let y_max = d3.max(data, function(d) { 
-	// 		return d.average_daily_visit
-	// 	})
-
-	// 	let y = d3.scaleLinear()
-	// 		.domain([0,y_max+(y_max/100*10)]) 
-	// 		.range([height-margin.top,0])
-
-	// 	d3.select(".yAxis")
-	// 		.transition()
-	// 		.call(d3.axisLeft(y));
-	// }
-
 }
 
-$( "#subjects" ).change(function() {
-	let subject = this.value;
-	dv1(subject)
-});
 
 $(document).ready(function() {
-	dv1("Biologia");
+	let random_subject = (Math.floor(Math.random() * 17) + 0) + 1
+	// console.log(random_subject, subjects[random_subject]);
+
+	document.getElementById("subjects").selectedIndex = random_subject;
+	// const select = $("#subjects");
+    // select.selectedIndex = random_subject;
+
+	dv1(subjects[random_subject]);
 });
+
