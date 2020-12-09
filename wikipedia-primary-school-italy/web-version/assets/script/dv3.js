a01d1d3597c2285995bd4399f75cbe7489de2967
giovannipro <giovanni.profeta@supsi.ch>
2020-12-09 22:48:33 +0100

    update author dataset

--- wikipedia-primary-school-italy/web-version/assets/script/dv3.js
+++ wikipedia-primary-school-italy/web-version/assets/script/dv3.js
@@ -8,34 +8,60 @@ function escape_(item){
 	let output = item.replace("'","%27")
 	return output
 }
-console.log(escape_("Guido,_i'_vorrei_che_tu_e_Lapo_ed_io (ws it),"))
+// console.log(escape_("Guido,_i'_vorrei_che_tu_e_Lapo_ed_io (ws it),"))
 
 function dv3() {
-	d3.tsv("../assets/data/publications.tsv").then(loaded);
+	Promise.all([
+		d3.tsv("../assets/data/publications.tsv"),
+		d3.tsv("../assets/data/authors.tsv"),
+	])
+	// d3.tsv("../assets/data/publications.tsv")
+	.then(loaded);
 
 	function loaded(data) {
-		// console.log(data)
+		// console.log(data[0],data[1])
+		
+		// let merged_data = data[0].map((item, i) => Object.assign({}, item, data[1][i]));
+		
+		let merged_data = data[0].map((e) => {
+		    for(let element of data[1]){
+		        if(e.author == element.author) Object.assign(e, element);
+		    }
+		    return e;
+		});
+
+		console.log(merged_data);
 
 		let output = "";
 
 		let author_group = d3.nest()
 			.key(d => d.author)
-			.entries(data)
+			.entries(merged_data)
 		console.log(author_group)
 
 		publication_sort = author_group.sort(function(a,b){
 			return b.values.length - a.values.length;
 		})
 
+		let count = 1;
 		publication_sort.forEach(function (d,i) {
 			// console.log(d.values[0].period)
 
-			output += "<a href='" + wiki_link + d.key + "' target='_blank'>" + d.key + "</a> (" + d.values[0].period + ") - ";
-			output += d.values.length + " pubblicazioni<br/><ul style='margin: 0 0 0 40px;'>";
+			output += count + ") <a href='" + wiki_link + d.key + "' target='_blank'>" + d.key + "</a> (" + d.values[0].period + ") - ";
+			output += (d.values.length) + " pubblicazioni<br/><ul style='margin: 0 0 0 40px;'>";
 
+			let test = "";
 			d.values.forEach(function (a,b) {
 
+				// test += a.pubb_w + a.pubbl_it + a.pubbl_la
+
 				output += "<li>";
+
+				// if (test == "---") {
+				// 	output += test 
+				// }
+
+				// test = ""
 
 				if (a.pubb_w != "-" && a.pubb_w != "deest" && a.pubb_w != "/" && a.pubb_w != " ") {
 					output += "<a href='"+ wiki_link + escape_(a.pubb_w) + "' target='_blank'>" + a.pubb_w + " (w it)</a>, "
@@ -53,7 +79,8 @@ function dv3() {
 
 			})
 
-			output += "</ul><br/>"
+			output += "</ul><br/>";
+			count += 1;
 		})
 
 		$("#dv3").append(output)
