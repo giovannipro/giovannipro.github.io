409bccba7b80255e3f21b64e5b411f30092a985c
giovannipro <giovanni.profeta@supsi.ch>
2021-02-13 20:43:58 +0100

    filter by population

--- itwiki-patrimonio-italiano/assets/script/dv1.js
+++ itwiki-patrimonio-italiano/assets/script/dv1.js
@@ -4,9 +4,68 @@ const map_contaier = "map1";
 
 const place_color = "#2a35f7"
 
+const population_range = [{
+	"a": {
+		"min": 0,
+		"max": 1000
+	},
+	"b": {
+		"min": 1001,
+		"max": 3000
+	},
+	"c": {
+		"min": 3001,
+		"max": 5000
+	},
+	"d": {
+		"min": 5001,
+		"max": 10000
+	},
+	"e": {
+		"min": 10001,
+		"max": 50000
+	},
+	"f": {
+		"min": 50001,
+		"max": 3000000
+	}
+}]
+
+function population(value){
+	if (value == 0){
+		p_min = population_range[0].a.min;
+		p_max = population_range[0].f.max;
+	}
+	else if (value == 1){
+		p_min = population_range[0].a.min;
+		p_max = population_range[0].a.max;
+	}
+	else if (value == 2){
+		p_min = population_range[0].b.min;
+		p_max = population_range[0].b.max;
+	}
+	else if (value == 3){
+		p_min = population_range[0].c.min;
+		p_max = population_range[0].c.max;
+	}
+	else if (value == 4){
+		p_min = population_range[0].d.min;
+		p_max = population_range[0].d.max;
+	}
+	else if (value == 5){
+		p_min = population_range[0].e.min;
+		p_max = population_range[0].e.max;
+	}
+	else {
+		p_min = population_range[0].f.min;
+		p_max = population_range[0].f.max;
+	}
+	return [p_min, p_max]
+}
+
+
 // make the visualization
 function dv1(){
-	console.log("start");	
 
 	let map = L.map(map_contaier, {
 		center: [41.9028, 12.9164],
@@ -24,9 +83,9 @@ function dv1(){
   		.then(function(data) {
 			console.log(data);
 
-			let the_population_min = 30;
-			let the_population_max = 2000;
-			console.log(the_population_min,the_population_max)
+			// let the_population_min = 30;
+			// let the_population_max = 2000;
+			// console.log(the_population_min,the_population_max)
 
 			// filter by region
 			// filter_region = data.filter(function(a,b){ 
@@ -35,11 +94,13 @@ function dv1(){
 			// console.log(filter_region.length)
 
 			// filter by population
+			let p_min = population_range[0].a.min
+			let p_max = population_range[0].a.max
 			filter_population = data.filter(function(a,b){ 
-				return a.Popolazione >= the_population_min && a.Popolazione <= the_population_max
+				return a.Popolazione >= p_min && a.Popolazione <= p_max
 			})
-			console.log(filter_population.length)
-
+			// console.log(p_min,p_max)
+			// console.log(filter_population.length)
 
 			let issue_min = d3.min(filter_population, function(d) { 
 				return Math.sqrt(+d.aNum/3.14);
@@ -49,8 +110,6 @@ function dv1(){
 			})
 
 			function append_markers(dataset){
-
-				// var bounds = L.latLngBounds()
 
 				let bounds = [];
 
@@ -97,7 +156,7 @@ function dv1(){
 				map.fitBounds(bounds, {
 					"padding": [50, 50],
 					"animate": true,
-				    "duration": 2
+				    "duration": 1
 				});
 			}
 
@@ -120,8 +179,33 @@ function dv1(){
 
 				// filter by population
 				filter_population = filter_region.filter(function(a,b){ 
-					return a.Popolazione > inhabitants
+					return a.Popolazione >= population(inhabitants)[0] && a.Popolazione <= population(inhabitants)[1]
 				})
+
+				$(".place").remove();
+				append_markers(filter_population);
+			});
+
+			$("#inhabitants").change(function() {
+				let inhabitants = this.value;
+				let region =  $("#region option:selected").val();
+				// console.log(region, inhabitants)
+
+				// filter by region
+				if (region !== "all"){
+					filter_region = data.filter(function(a,b){ 
+						return a.Regione == region
+					})
+				}
+				else {
+					filter_region = data
+				}
+
+				// filter by population
+				filter_population = filter_region.filter(function(a,b){ 
+					return a.Popolazione >= population(inhabitants)[0] && a.Popolazione <= population(inhabitants)[1]
+				})
+				console.log(population(inhabitants)[0], population(inhabitants)[1], filter_population.length)
 
 				$(".place").remove();
 				append_markers(filter_population);
