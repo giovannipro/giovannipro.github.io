9a9475f39ab801652957a1dfb5aa4f3a3860d51f
giovannipro <giovanni.profeta@supsi.ch>
2021-10-14 20:46:04 +0200

    define filter and sidebar

--- paravicini/assets/script.js
+++ paravicini/assets/script.js
@@ -1,6 +1,7 @@
 // map
 let map_contaier = "my_map";
 let min_zoom = 9;
+let map_center = [45.481, 8.9852];
 
 // markers
 let myIcon = L.icon({ // red green  // https://github.com/pointhi/leaflet-color-markers
@@ -13,8 +14,9 @@ let myIcon = L.icon({ // red green  // https://github.com/pointhi/leaflet-color-
 });
 
 function tsvJSON(tsv) {
-	const lines = tsv.split('\n');
+	const lines = tsv.split('\r');
 	const headers = lines.slice(0, 1)[0].split('\t');
+	// console.log(headers)
 	return lines.slice(1, lines.length).map(line => {
 	  const data = line.split('\t');
 	  return headers.reduce((obj, nextKey, index) => {
@@ -24,10 +26,9 @@ function tsvJSON(tsv) {
 	});
 }
 
-
 // load data
 function load_data(){
-	fetch("assets/data.tsv")
+	fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQGrLjvEojUsJnXde5dY3KB9Mw8fSJZXsU9QGMq0-RNoLbcLyJlgYaUvU0DByCA78kpIYXDKmHc8dE3/pub?gid=0&single=true&output=tsv")
 		.then(response => response.text())
 		.then((data) => {
 			load_map(data)
@@ -39,8 +40,10 @@ function load_map(data){
 	const buildings = tsvJSON(data);
 	console.log(buildings)
 
+	let info_bar = document.getElementById("info_bar");
+
 	let map = L.map(map_contaier, {
-		center: [45.881, 8.9852],
+		center: map_center,
 		zoom: min_zoom
 	});
 
@@ -49,7 +52,8 @@ function load_map(data){
 		maxZoom: 17,
 		minZoom: min_zoom,
 		tileSize: 256
-	}).addTo(map);
+	})
+	.addTo(map);
 
 	let category = "paravicini"
 
@@ -59,31 +63,18 @@ function load_map(data){
 	// append markers
 	function append_markers(category){
 
-		filter.addEventListener ("change", function () {
-			category = this.value;
-			// console.log(category);
-
-	       	document.querySelectorAll('.building').forEach(function(a){
-				a.remove()
-			})
-			append_markers(category)
-		});
-
 		let min_lat = 45.4; 
 		let max_lat = 46.5; 
 		let min_lon = 8.5; 
 		let max_lon = 9.5; 
 
 		let bounds = [];
-
 		buildings.forEach(function(entry){
 
 			let name = entry.name;
 			let lat = entry.lat;
 			let lon = entry.lon;
-			let ref = entry.ref;
 			let cat = entry.category;
-			let link = entry.link;
 
 			let tooltip_text = "<span>" +
 				"<strong>" + name + "</strong>" +
@@ -93,47 +84,87 @@ function load_map(data){
 				icon: myIcon,
 				id:  buildings
 			})
-			// .bindPopup(popup_text , {
-			// 	maxWidth: 300
+		 //   	.bindTooltip( tooltip_text , {
+			// 	permanent: false,
+			// 	interactive: true,
+			// 	noWrap: false,
+			// 	opacity: 0.9,
 			// })
-		   	.bindTooltip( tooltip_text , {
-				permanent: false,
-				interactive: true,
-				noWrap: false,
-				opacity: 0.9,
+			.bindPopup(tooltip_text, {
+				"maxWidth": 200,
+				"className": "popup"
 			})
 			.addTo(map)
 			.on('click', onClick);
-			L.DomUtil.addClass( building._icon, "building" );
-			L.DomUtil.addClass( building._shadow, "building" );
-		
-			// bounds.push([lat,lon])
+			L.DomUtil.addClass( building._icon, "building " + cat);
+			L.DomUtil.addClass( building._shadow, "building " + cat);
+			L.DomUtil.addClass( building._icon, "building " + cat);
 
-			// map.fitBounds(bounds, {
-			// 	"padding": [80, 80],
-			// 	"animate": true,
-			//     "duration": 2
-			// });	
-			
+			bounds.push([lat,lon])
+			map.fitBounds(bounds, {
+				"padding": [140, 200],
+				"animate": true,
+			    "duration": 2
+			});	
+
 			function onClick(){
-				// window.location = link; 
-				open_sidebar(tooltip_text);
+				open_sidebar(entry);
 
+		  //       let theIcon = L.icon({
+		  //           iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+				// 	shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+		  //       });
+		  //       this.setIcon(theIcon);
 			}
+
 		})
 
+		filter.addEventListener ("change", function () {
+			category = this.value;
+
+			document.querySelectorAll(".popup").forEach(function(a){
+				a.classList.toggle("popup_close");
+			})
+
+	       	document.querySelectorAll('.building').forEach(function(a){
+	       		if (a.className.indexOf(category) !== -1){
+	       			a.style.display = "block";
+	       		}
+	       		else {
+	       			a.style.display = "none";
+	       		}
+			})
+
+	       	info_bar.innerHTML = "<div id='info'>Seleziona un punto sulla mappa</div>";
+
+		})
 	}
 	append_markers(category);
 
 	// sidebar
-	function open_sidebar(item){
-		const container = document.getElementById("info_bar");
-		const title = 
+	function open_sidebar(entry){
+		info_bar.innerHTML = "";
+		info_bar.style.display = "block";
 
-		container.style.display = "block";
+		let name = entry.name;
+		let lat = entry.lat;
+		let lon = entry.lon;
+		let ref = entry.ref;
+		let cat = entry.category;
+		let link = entry.link;
+
+		let the_link = "";
+		if (link.length > 20){
+			the_link = "<a href='" + link +"' title='" + name + "'>maggiori informazioni</a>"
+		}
+
+		let output = "<div id='info'>" +
+			"<span class='b_name'>" + name + "</span><br/><br/>" + 
+			ref + "<br/></br>" +
+			the_link + 
+			"</div>"
 		
-		container.innerHTML = "";
-		container.append(item)
+		info_bar.innerHTML = output;
 	}
 }
 
