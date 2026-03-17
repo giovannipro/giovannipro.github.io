48a44776a2f854a300f8942aaf3acf5fe6d9a9e1
giovannipro <g.profet@gmail.com>
2026-03-17 14:31:07 +0100

    add some interaction

--- map-design/assets/script/script.js
+++ map-design/assets/script/script.js
@@ -1,15 +1,21 @@
-let map = L.map('map')
-    .setView([45.873673081319794, 8.979357135658603], 13);
+// basic setup ----------------------
+
+let map = L.map('map', {
+    center: [45.873673081319794, 8.979357135658603],
+    zoom: 13
+});
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     })
     .addTo(map);
 
+
+// markers ----------------------
+
 L.marker([45.87, 8.97])
     .addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    // .openPopup();
 
 let circle = L.circle([45.88, 8.96], {
     color: 'red',
@@ -25,7 +31,20 @@ let polygon = L.polygon([
     [45.89, 8.98]
 ]).addTo(map);
 
-let popup = L.popup()
-    .setLatLng([45.87, 8.965])
-    .setContent("I am a standalone popup.")
-    .openOn(map);
+// interactivity ----------------------
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+map.on('click', onMapClick);
+
+let popup = L.popup();
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent(e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
